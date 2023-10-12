6019b4f8341ae04c7b7149d3bacfc590b03008e9
Julian Markow <jomarkow@gmail.com>
2023-10-11 21:45:41 -0300

    Add tab and note functions

--- src/interface.py
+++ src/interface.py
@@ -5,6 +5,10 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
+class Tab(customtkinter.CTk):
+    
+    def __init__():
+        pass
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -47,53 +51,60 @@ class App(customtkinter.CTk):
         self.frames = {}
 
         self.tabview = customtkinter.CTkTabview(self, width=250)
-        self.tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.tabview.grid(row=0, rowspan = 3, column=1, columnspan = 3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        self.tabview.add("default") 
-        self.frames["default"] = customtkinter.CTkFrame(self.tabview.tab("default"))
-       
         # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="New note text")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
-
-        self.main_button_1 = customtkinter.CTkButton(master=self, text = "Add note", fg_color="transparent", command = self.new_note(self.frames[self.tabview.get()]))
+        self.main_button_1 = customtkinter.CTkButton(master=self, text = "Add note", fg_color="black", command = self.new_note)
         self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
 
         # create tabview
 
 
+        self.note_cant = 0
+
+    def new_note(self):
+        
+        actual_tab = self.tabview.get()
 
 
-    def new_note(self, master):
-
-        self.tabs[self.tabview.get()] = customtkinter.CTkCheckBox(master= master)
-        self.tabs[self.tabview.get()].grid(row=1, column=0, pady=(20, 0), padx=20, sticky="s")
-
-
-
-
+        self.note = customtkinter.CTkCheckBox(master= self.tabview.tab(actual_tab), text = self.entry.get(), font = ("Arial", 20), command=self.delete_note)
+        self.note.grid(row=self.note_cant, column=0, pady=(20, 20), padx=20, sticky="s")
+        self.note_cant += 1
+        
+        
+    def delete_note(self):
+        
+        self.note.destroy()
+    
     def add_tab(self):
-        dialog = customtkinter.CTkInputDialog(text="Type tab number:", title="New tab")
-        self.tabview.add(dialog.get_input()) 
-        self.tabs[dialog.get_input()] = []
-
-        self.frames[dialog.get_input()] = customtkinter.CTkFrame(self.tabview.tab(dialog.get_input()))
         
-        self.frames[dialog.get_input()].grid(row=0, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
-
+        get_tab_name = customtkinter.CTkInputDialog(text="Type tab name:", title="New tab")
         
+        tab_name = get_tab_name.get_input()
+        self.tabview.add(tab_name) 
+        
+        
+        frame = customtkinter.CTkFrame(self.tabview.tab(tab_name))
+        
+        #self.tabs[tab_name] = customtkinter.CTkFrame(self.tabview.tab(dialog.get_input()))
+        
+        #self.frames[dialog.get_input()].grid(row=0, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+
+    
 
 
 
         # create checkbox and switch frame
 
-        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="s")
-        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
+        #self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        #self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="s")
+        #self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        #self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
+        #self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        #self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
 
         # set default values
 
